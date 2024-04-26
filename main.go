833b2759f44020f5bf56fc1b2989ba90da47c632
Dennis Schoepf <me@dnsc.io>
2024-04-26 13:14:01 +0200

    Adds dummy status endpoint

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Init freed application")
+	r := gin.Default()
+
+	r.GET("/status", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"application": "freed",
+			"version":     "0.0.1",
+			"status":      "up",
+		})
+	})
+
+	r.Run(":42069")
 }
