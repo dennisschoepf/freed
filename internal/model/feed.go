99bd43128e269eb4b6b747d55d34d22f8d3f3d92
Dennis <me@dnsc.io>
2024-05-14 22:52:44 +0200

    Uses correct userID data type

--- internal/model/feed.go
+++ internal/model/feed.go
@@ -4,5 +4,5 @@ type Feed struct {
 	ID     int64  `json:"id"`
 	Name   string `json:"name"`
 	Url    string `json:"url" validate:"required,url"`
-	UserID int64  `json:"userId" validate:"required"`
+	UserID string `json:"userId" validate:"required"`
 }
