96fc2340681aa35e814a826877170736750dd785
Dennis <me@dnsc.io>
2024-05-14 22:35:33 +0200

    Adapts models

--- internal/model/article.go
+++ internal/model/article.go
@@ -4,10 +4,10 @@ import "time"
 
 // TODO: Tag for article?
 type Article struct {
-	ID     int64
-	Name   string
-	Url    string
-	Read   bool
-	ReadAt time.Time
-	FeedID *int64
+	ID     int64     `json:"id"`
+	Name   string    `json:"name"`
+	Url    string    `json:"url" validate:"required,url"`
+	Read   bool      `json:"read"`
+	ReadAt time.Time `json:"readAt"`
+	FeedID *int64    `json:"feedId"`
 }
--- internal/model/feed.go
+++ internal/model/feed.go
@@ -1,8 +1,8 @@
 package model
 
 type Feed struct {
-	ID     int64
-	Name   string
-	Url    string
-	UserID int64
+	ID     int64  `json:"id"`
+	Name   string `json:"name"`
+	Url    string `json:"url" validate:"required,url"`
+	UserID int64  `json:"userId" validate:"required"`
 }
