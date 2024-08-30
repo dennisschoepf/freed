051e84de868641bca5e2ad7737cfd1494b10a81d
Dennis <me@dnsc.io>
2024-08-31 01:41:44 +0200

    Removes unused migrations

--- internal/database/migrations/1_feed.sql
+++ internal/database/migrations/1_feed.sql
@@ -3,8 +3,7 @@ CREATE table feed (
   id INTEGER PRIMARY KEY,
   name text NOT NULL,
   url text NOT NULL UNIQUE,
-  userId text NOT NULL,
-  FOREIGN KEY (userId) REFERENCES user(id)
+  type text NOT NULL
 );
 
 -- +migrate Down
