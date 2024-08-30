489d3b88463d80a9ac1d6459f4465e8bfcf09fd4
Dennis <me@dnsc.io>
2024-08-30 14:59:54 +0200

    Removes json annotated model structs

--- internal/model/user.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type User struct {
-	ID        string `json:"id"`
-	FirstName string `json:"firstName" validate:"required"`
-	Email     string `json:"email" validate:"required,email"`
-}
