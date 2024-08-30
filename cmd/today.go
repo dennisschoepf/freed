db7ed2fa3b513824b1e4a42a53e25ac0de8d5d59
Dennis <me@dnsc.io>
2024-08-30 17:06:43 +0200

    Adds subcommands

---
+++ cmd/today.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// todayCmd represents the today command
+var todayCmd = &cobra.Command{
+	Use:   "today",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		dbPath := viper.GetString("path")
+		fmt.Println(dbPath)
+		fmt.Println("today called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(todayCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// todayCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// todayCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
