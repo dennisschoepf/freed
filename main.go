727a788d44d44bb4cda03562b45e5db508eda14b
Dennis <me@dnsc.io>
2024-08-30 17:07:38 +0200

    Cobra initialization in main.go

--- main.go
+++ main.go
@@ -1,28 +1,22 @@
+/*
+Copyright © 2024 Dennis Schoepf dev@dnsc.io
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	_ "embed"
-	"fmt"
-	"freed/internal/database"
-	"log"
-	"os"
-
-	_ "github.com/joho/godotenv/autoload"
-)
+import "freed/cmd"
 
 func main() {
-	dbFile := os.Getenv("DB_PATH")
-
-	if dbFile == "" {
-		log.Fatalf("No ENV value set for 'DB_PATH', could not initialize database. Please provide a valid path and filename")
-	}
-
-	_, err := database.Connect(dbFile)
-
-	if err != nil {
-		log.Fatalf("Could not initialize database: %v", err)
-	}
-
-	fmt.Println("Setup finished")
-	os.Exit(1)
+	cmd.Execute()
 }
