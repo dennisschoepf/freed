6151e9ed80fb693b73efbc3477ded8988699571d
Dennis <me@dnsc.io>
2024-09-03 20:29:03 +0200

    Adapts feed model for database to new table schema

--- internal/database/feed.go
+++ internal/database/feed.go
@@ -2,18 +2,13 @@ package database
 
 type FeedType string
 
-const (
-	RSS FeedType = "RSS"
-)
-
 type Feed struct {
-	Name     string
-	Url      string
-	FeedType FeedType
+	Name string
+	Url  string
 }
 
 func (f Feed) Insert() (int64, error) {
-	result, err := db.Exec("INSERT INTO feed (name, url, type) VALUES (?,?,?)", f.Name, f.Url, f.FeedType)
+	result, err := db.Exec("INSERT INTO feed (name, url) VALUES (?,?)", f.Name, f.Url)
 
 	if err != nil {
 		return 0, err
